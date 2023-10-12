d718b25ea24a6b81d2ef2cb4eda2be5c285f1d37
alif33 <alifhasan332@gmail.com>
2023-10-12 18:29:37 +0100

    responsive fix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,9 +12,9 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="bg-white h-screen overflow-y-hidden">
       <Header/>
-      <div className="h-[calc(100vh-4rem)] grid grid-cols-12 mx-auto px-5">
+      <div className="h-[calc(100vh-4rem)] grid grid-cols-12 mx-auto px-0 sm:px-0 md:px-5">
         <LeftSidebar/>
-        <div className="h-[calc(100vh-4rem)] col-span-12 sm:col-span-12 md:col-span-7 bg-[#FAFAFA] rounded-xl ml-2">
+        <div className="h-[calc(100vh-4rem)] col-span-12 sm:col-span-12 md:col-span-7 bg-[#FAFAFA] rounded-xl ml-0 md:ml-2">
           <div className="flex flex-col">
             <div className="h-[calc(100vh-12rem)] overflow-y-scroll">{children}</div>
             <SearchBar/>
